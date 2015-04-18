79dac042a4e3e2ef49f8bec3e9b30a3615e4fd7f
Pine Mizune <pinemz@gmail.com>
2015-04-18 23:48:49 +0900

    Windows で Node.js が PATH から見つからない時に、自動ダウロードする機能を追加

--- bin/install.ps1
+++ bin/install.ps1
@@ -1,4 +1,9 @@
 $node_bin = ''
+$node_version = 'v0.10.38'
+$node_arch = 'x64'
+$node_sha256 = 'e82b7c6a102ae3266149b3af48ccb1d15c7438ff30a3109735148b38ed271fc1'
+$node_repo = 'https://nodejs.org/dist/'
+
 
 $cwd = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
@@ -8,7 +13,23 @@ if ($node_cmd = Get-Command 'node' -ErrorAction SilentlyContinue) {
 
 if ($node_bin -eq '') {
   Write "Can't find Node.js (in PATH)"
-  return
+  
+  $node_bin_url = "$node_repo/$node_version/$node_arch/node.exe"
+  $node_bin = "$cwd\node.exe"
+  
+  
+  if (-not(Test-Path "$node_bin")) {
+    Write "Downloading Node.js using HTTPS"
+    Write "Downloading from: $node_bin_url"
+    
+    Invoke-WebRequest -OutFile "$node_bin" -Uri "$node_bin_url"
+    $hash = (Get-FileHash -LiteralPath "$node_bin" -Algorithm SHA256).Hash
+    
+    if ($node_sha256 -ne $hash) {
+      Write-Error "Hash mismatch"
+      return
+    }
+  }
 }
 
 Write "> ""$node_bin"" ""$cwd\install.js"""
