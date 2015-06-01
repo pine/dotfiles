2bbff7aadaefffd874fc675dae551369888c84bf
Pine Mizune <pinemz@gmail.com>
2015-06-01 18:05:24 +0900

    Support cURL

--- bin/install.bash
+++ bin/install.bash
@@ -14,19 +14,24 @@ if [ "$node_bin" = "" ]; then
     if [ -e "$HOME/nodebrew" ]; then
       rm -rf "$HOME/nodebrew"
     fi
-    
-    wget git.io/nodebrew -O ~/nodebrew
+
+    if type wget > /dev/null 2>&1; then
+      wget git.io/nodebrew -O ~/nodebrew
+    else
+      curl -L http://git.io/nodebrew > ~/nodebrew
+    fi
+
     perl ~/nodebrew setup
-    
+
     rm -rf "$HOME/nodebrew"
   fi
-  
+
   nodebrew_bin="$HOME/.nodebrew/current/bin/nodebrew"
-  
+
   if [ -e "$nodebrew_bin" ]; then
     "$nodebrew_bin" install-binary $node_version
     "$nodebrew_bin" use $node_version
-    
+
     if [ -e "$HOME/.nodebrew/current/bin/node" ]; then
       node_bin="$HOME/.nodebrew/current/bin/node"
     fi
@@ -36,7 +41,7 @@ fi
 if [ "$node_bin" != "" ]; then
   echo "> $node_bin -v"
   "$node_bin" -v
-  
+
   echo "> $node_bin $cwd/install.js"
   "$node_bin" "${cwd}/install.js"
 else
