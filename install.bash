ebece157a8b12ca8a7623b2266827fb29cce9981
Pine Mizune <pinemz@gmail.com>
2016-07-20 17:05:16 +0000

    fix(install.bash): add install script

---
+++ install.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+if uname | fgrep -i Darwin > /dev/null 2>&1; then
+    echo "Darwin detected"
+else
+    echo "Linux detected"
+    set -x
+
+    sudo apt-get update -y
+    sudo apt-get install git -y
+
+    mkdir -p ~/project
+    cd ~/project
+
+    git clone https://github.com/pine/dotfiles.git
+    cd dotfiles
+
+    git checkout v2
+    bash ,/bin/install.bash
+
+    set +x
+fi
