8ba1b3421f090801f77195570fca6f7008caa4d6
Pine Mizune <pinemz@gmail.com>
2018-05-08 11:16:39 +0900

    feat(home): update fish config

---
+++ resources/home/.config/fish/functions/docker-compose.fish
@@ -0,0 +1,1 @@
+/Users/kazuki.matsushita/.config/fisherman/docker-fish-completion/docker-compose.fish
---
+++ resources/home/.config/fish/functions/gen_docker_fish_completions.py
@@ -0,0 +1,1 @@
+/Users/kazuki.matsushita/.config/fisherman/docker-fish-completion/gen_docker_fish_completions.py
