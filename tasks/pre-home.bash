14c823aff2a63e84d24225db1490d3d2c597047e
Pine Mizune <pinemz@gmail.com>
2018-07-29 19:45:22 +0900

    fix(pre-home.bash): fix bug

--- tasks/pre-home.bash
+++ tasks/pre-home.bash
@@ -5,7 +5,7 @@ home_preinstall() {
   local mkdirp="$DOTFILES_CONFIG/home/mkdirp.conf"
 
   cat "$mkdirp" | while read dir; do
-    echo "> mkdir -p ~/$dir"
-    mkdir -p "~/$dir"
+    echo "> mkdir -p $HOME/$dir"
+    mkdir -p "$HOME/$dir"
   done
 }
