1598a55050d7e98e06407db6f45004ebd6cff1a0
Pine Mizune <pinemz@gmail.com>
2018-07-31 17:54:34 +0900

    fix(home.bash): support secured files

--- tasks/home.bash
+++ tasks/home.bash
@@ -3,13 +3,26 @@
 home_install() {
   local file
   local files="$DOTFILES_CONFIG/home/files.conf"
+  local secured_files="$DOTFILES_SECURED_CONFIG/home/files.conf"
   local src
 
-  cat $files | while read file; do
+  # Process files
+  cat "$files" | while read file; do
     src="$DOTFILES_RESOURCE/home/$file"
 
     echo "> ln -s $src ~/$file"
     rm -f "~/$file"
     ln -s "$src" "~/$file"
   done
+
+  # Process secured files
+  if [ -r "$secured_files" ]; then
+    cat "$secured_files" | while read file; do
+      src="$DOTFILES_SECURED_RESOURCES/home/$file"
+
+      echo "> ln -s $src ~/$file"
+      rm -f "~/$file"
+      ln -s "$src" "~/$file"
+    done
+  fi
 }
