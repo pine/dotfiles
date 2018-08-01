c01356ad4995afb43f48564a0b9bee72b3e915cb
Pine Mizune <pinemz@gmail.com>
2018-08-01 23:32:24 +0900

    feat(home): add fish config

---
+++ resources/home/.config/fish/completions/ndenv.fish
@@ -0,0 +1,126 @@
+# ~/.config/fish/completions/ndenv.fish
+
+# node-build binary is used from pyenv completion
+function node-build
+  if test -n "$NDENV_ROOT"
+    eval $NDENV_ROOT/plugins/node-build/bin/node-build $argv
+  else
+    eval $HOME/.anyenv/envs/ndenv/plugins/node-build/bin/node-build $argv
+  end
+end
+
+function __fish_ndenv_needs_command
+  set cmd (commandline -opc)
+  if [ (count $cmd) -eq 1 -a $cmd[1] = 'ndenv' ]
+    return 0
+  end
+  return 1
+end
+
+function __fish_ndenv_using_command
+	set cmd (commandline -opc)
+	if [ (count $cmd) -gt 1 ]
+		if [ $argv[1] = $cmd[2] ]
+			return 0
+		end
+	end
+	return 1
+end
+
+function __fish_ndenv_executables
+	ndenv exec --complete
+end
+
+function __fish_ndenv_installed_nodes
+	ndenv versions --bare
+end
+
+function __fish_ndenv_official_nodes
+	node-build --definitions
+end
+
+function __fish_ndenv_prefixes
+	ndenv prefix --complete
+end
+
+### commands
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a commands -d 'List all ndenv commands'
+complete -f -c ndenv -n '__fish_ndenv_using_command commands' -a '--complete --sh --no-sh'
+
+### completions
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a completions
+
+### exec
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a exec
+complete -f -c ndenv -n '__fish_ndenv_using_command exec' -a '(__fish_ndenv_executables)'
+
+### global
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a global -d 'Set or show the global node version'
+complete -f -c ndenv -n '__fish_ndenv_using_command global' -a '(__fish_ndenv_installed_nodes)'
+
+### help
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a help
+
+### hooks
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a hooks
+
+### init
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a init
+
+### install
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a install -d 'Install a node version'
+complete -f -c ndenv -n '__fish_ndenv_using_command install' -a '(__fish_ndenv_official_nodes)'
+
+### local
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a local -d 'Set or show the local directory-specific node version'
+complete -f -c ndenv -n '__fish_ndenv_using_command local' -a '(__fish_ndenv_installed_nodes)'
+
+### prefix
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a prefix -d 'Shows a node version installed folder'
+complete -f -c ndenv -n '__fish_ndenv_using_command prefix' -a '(__fish_ndenv_prefixes)'
+
+### rehash
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a rehash -d 'Rehash ndenv shims (run this after installing binaries)'
+
+### root
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a root -d 'ndenv root folder'
+
+### shell
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a shell -d 'Set or show the shell-specific node version'
+complete -f -c ndenv -n '__fish_ndenv_using_command shell' -a '--unset (__fish_ndenv_installed_nodes)'
+
+### shims
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a shims
+complete -f -c ndenv -n '__fish_ndenv_using_command shims' -a '--short'
+
+### version
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a version  -d 'Show the current node version'
+
+### version-file
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a version-file
+
+### version-file-read
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a version-file-read
+
+### version-file-write
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a version-file-write
+
+### version-name
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a version-name
+
+### version-origin
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a version-origin
+
+### versions
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a versions -d 'List all node versions known by ndenv'
+
+### whence
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a whence -d 'List all node versions with the given command'
+complete -f -c ndenv -n '__fish_ndenv_using_command whence' -a '--complete --path'
+
+### which
+complete -f -c ndenv -n '__fish_ndenv_needs_command' -a which -d 'Show the full path for the given node command'
+complete -f -c ndenv -n '__fish_ndenv_using_command which' -a '(__fish_ndenv_executables)'
+
+
+# vim: se sw=2 ts=2 sts=2 et ft=fish :
---
+++ resources/home/.config/fish/config.fish
@@ -0,0 +1,100 @@
+# ~/.config/fish/config.fish
+
+# env -------------------------------------------------------------------------
+
+set -g CDPATH .
+test -d ~/project; and set -g CDPATH $CDPATH ~/project
+
+begin
+  set -l paths /sbin /usr/sbin /bin /usr/sbin \
+    /usr/local/sbin /usr/local/bin \
+    ~/Library/Android/sdk/tools/bin ~/Library/Android/sdk/platform-tools \
+    /usr/lib/dart/bin /usr/local/Homebrew/bin /usr/local/*/bin /usr/local/opt/mysql@5.7/bin \
+    ~/bin ~/bin/*/bin
+
+  for p in $paths
+    test -d $p; and set -x PATH $p $PATH
+  end
+end
+
+set -x LANG en_US.UTF-8
+set -x LC_TIME C
+set -x LC_LC_NUMERIC C
+
+if test -d ~/Library/Android/sdk/
+  set -x ANDROID_HOME ~/Library/Android/sdk/
+end
+
+# history ---------------------------------------------------------------------
+
+function history-merge --on-event fish_preexec
+	history --save
+	history --merge
+end
+
+
+# anyenv ----------------------------------------------------------------------
+
+if test -d ~/.anyenv
+  set -x PATH $HOME/.anyenv/bin $PATH
+end
+
+# rbenv
+if test -d ~/.anyenv/envs/rbenv
+  set -x RBENV_ROOT "$HOME/.anyenv/envs/rbenv"
+  set -x PATH "$RBENV_ROOT/bin" $PATH
+  status --is-interactive; and . (rbenv init - | psub)
+end
+
+# plenv
+if test -d ~/.anyenv/envs/plenv
+  set -x PLENV_ROOT "$HOME/.anyenv/envs/plenv"
+  set -x PATH "$PLENV_ROOT/bin" $PATH
+  status --is-interactive; and . (plenv init - | psub)
+end
+
+# swiftenv
+if test -d ~/.anyenv/envs/swiftenv
+  set -x SWIFTENV_ROOT "$HOME/.anyenv/envs/swiftenv"
+  set -x PATH "$SWIFTENV_ROOT/bin" $PATH
+  status --is-interactive; and . (swiftenv init - | psub)
+end
+
+# scalaenv
+if test -d ~/.anyenv/envs/scalaenv
+  set -x SCALAENV_ROOT "$HOME/.anyenv/envs/scalaenv"
+  set -x PATH "$SCALAENV_ROOT/bin" $PATH
+  status --is-interactive; and . (scalaenv init - | psub)
+end
+
+# rustup
+if test -f ~/.cargo/env
+  source ~/.cargo/env
+end
+
+# vim -------------------------------------------------------------------------
+
+if type -p nvim > /dev/null 2>&1
+  set -Ux EDITOR nvim
+else if type -p vim > /dev/null 2>&1
+  set -Ux EDITOR vim
+else
+  set -Ux EDITOR vi
+end
+
+
+# git -------------------------------------------------------------------------
+
+set -Ux GIT_MERGE_AUTOEDIT no
+
+
+# -----------------------------------------------------------------------------
+
+for i in ~/.config/fish/postconf.d/* ;
+  source $i
+end
+
+set -U fish_user_paths (echo $fish_user_paths | tr ' ' '\n' | awk '!a[$0]++' -)
+set -gx PATH (echo $PATH | tr ' ' '\n' | awk '!a[$0]++' -)
+
+# vim: se sw=2 ts=2 sts=2 et ft=fish :
