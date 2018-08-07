7b23b528a3d8675967e30dbf3ab9e9c434aa1bf4
Pine Mizune <pinemz@gmail.com>
2018-08-07 20:54:24 +0900

    fix(anyenv): support *env plugin

--- tasks/anyenv.bash
+++ tasks/anyenv.bash
@@ -2,6 +2,7 @@
 
 anyenv_install() {
   _anyenv_install_envs
+  _anyenv_install_plugins
   _anyenv_update_envs
 }
 
@@ -13,6 +14,48 @@ _anyenv_install_envs() {
   cat "$envs" | while read env; do
     echo "> anyenv install -s $env"
     anyenv install -s $env
+  done
+}
+
+# Install plugins
+_anyenv_install_plugins() {
+  local env
+  local envs="$DOTFILES_CONFIG/anyenv/envs.conf"
+  local name
+  local plugin
+  local plugins
+  local repo_url
+  local repo_path
+
+  cat "$envs" | while read env; do
+    if [ ! -d "$HOME/.anyenv/envs/$env" ]; then
+      continue
+    fi
+
+    plugins="$DOTFILES_CONFIG/anyenv/plugins/$env.conf"
+    if [ ! -r "$plugins" ]; then
+      continue
+    fi
+
+    cat "$plugins" | while read plugin; do
+      plugin="$(echo $plugin | tr '\t' ' ')"
+
+      name=${plugin%% *}
+      if [ -z "$name" ]; then
+        continue
+      fi
+
+      repo_url=${plugin##* }
+      if [ -z "$repo_url" ]; then
+        continue
+      fi
+
+      repo_path="$HOME/.anyenv/envs/$env/plugins/$name"
+      if [ ! -d "$repo_path" ]; then
+        echo "> git clone $repo_url $repo_path"
+        git clone "$repo_url" "$repo_path"
+      fi
+    done
   done
 }
 
