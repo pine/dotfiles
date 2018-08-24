8bef8f983c6422ea2a45ce35a963146b072840aa
Pine Mizune <pinemz@gmail.com>
2018-08-25 02:26:46 +0900

    fix(config): add apt pkgs conf

--- tasks/apt.bash
+++ tasks/apt.bash
@@ -2,6 +2,27 @@
 
 apt_install() {
   if ! is-macos && has-apt; then
-    :
+    _apt_install_pkgs
   fi
 }
+
+
+_apt_install_pkgs() {
+  local pkgs="$DOTFILES_CONFIG/apt/pkgs.conf"
+  local pkg
+
+  cat "$pkgs" | while read pkg; do
+    if [ "${pkg:0:1}" == '#' ]; then
+      continue
+    fi
+
+    pkg="${pkg%%\#*}"
+    pkg="$(echo "$pkg" | tr -d '[:space:]')"
+
+    if ! dpkg -s "$pkg" > /dev/null; then
+      echo "> apt install $pkg -y"
+      apt install "$pkg" -y
+    fi
+  done
+}
+
