88e7d38d1773712e224f2ab68bdb16e6fe0ed499
Pine Mizune <pinemz@gmail.com>
2018-11-08 14:03:35 +0900

    fix(tasks/pref.bash): run apple script

--- tasks/pref.bash
+++ tasks/pref.bash
@@ -1,5 +1,15 @@
 # tasks/pref.bash
 
+
 pref_install() {
-  :
+  if is-macos; then
+    _pref_install_macos
+  fi
+}
+
+
+_pref_install_macos() {
+  local script="$DOTFILES_RESOURCES/pref/prefs.scpt"
+  echo "> osascript $script"
+  osascript "$script"
 }
