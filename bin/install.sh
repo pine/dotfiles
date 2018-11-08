f44d11c3499b4627bd3754a0d3795c2f4e8f7c9d
Pine Mizune <pinemz@gmail.com>
2018-11-08 10:58:13 +0900

    fix(bin): fix locale typo

--- bin/install.sh
+++ bin/install.sh
@@ -2,9 +2,9 @@
 
 set -eu -o pipefail
 
-export LANG=en_US.UTF8
-export LANGUAGE=en_US.UTF8
-export LC_ALL=en_US.UTF8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 # -------------------------------------------------------------------
 
