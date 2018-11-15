d12f2f6f4596e6dbf80ddf64bfe813d4a78a15b5
Pine Mizune <pinemz@gmail.com>
2018-11-15 17:46:24 +0900

    fix(pyenv): fix pyenv err

--- resources/home/.config/fish/postconf.d/pyenv.fish
+++ resources/home/.config/fish/postconf.d/pyenv.fish
@@ -1,5 +1,7 @@
 # ~/.config/fish/conf.d/pyenv.fish
 
+set -x PYTHON_CONFIGURE_OPTS="--enable-shared"
+
 if test -d ~/.anyenv/envs/pyenv
   set -x PYENV_ROOT "$HOME/.anyenv/envs/pyenv"
   set -x PATH "$PYENV_ROOT/bin" $PATH
