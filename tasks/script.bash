19ab007bb37406f4afb1a419fef068bec6f16bc0
Pine Mizune <pinemz@gmail.com>
2024-12-08 17:32:34 +0900

    refactor: use yaml config

--- tasks/script.bash
+++ tasks/script.bash
@@ -1,8 +1,8 @@
 tasks_script_install() {
-  local scripts="$DOTFILES_CONFIG/script/files.conf"
+  local config_path="$DF_CONFIG/script/files.yml"
   local secured_scripts="$DOTFILES_SECURED_CONFIG/script/files.conf"
 
-  _script_run_scripts "$DOTFILES_RESOURCES" "$scripts" 'script conf'
+  _script_run_scripts_yml "$DOTFILES_RESOURCES" "$config_path" 'main'
   _script_run_scripts "$DOTFILES_SECURED_RESOURCES" "$secured_scripts" 'secure script conf'
   _script_run_scripts "$CORPORATE_RESOURCES_DIR" "$CORPORATE_CONFIG_DIR/script/files.conf" 'corporate script conf'
 }
@@ -26,6 +26,25 @@ _script_run_scripts() {
 
       _script_run_script "$resources_dir" "$script"
     done 9< "$conf_path"
+  else
+    echo 'no'
+  fi
+}
+
+_script_run_scripts_yml() {
+  local resources_dir=$1
+  local config_path=$2
+  local repository=$3
+  local script_name
+
+  echo -n "Checking if $repository config exists ... "
+  if [ -f "$config_path" ]; then
+    echo 'yes'
+
+    cat "$config_path" | "$YQ_PATH" '.[]' -o=j -I=0 | while read script; do
+      script_name=$(echo "$script" | "$YQ_PATH" '.name')
+      _script_run_script "$resources_dir" "$script_name"
+    done
   else
     echo 'no'
   fi
