3f0b9734886de395987652af45c4db47d2228d6e
Pine Mizune <pinemz@gmail.com>
2025-07-08 16:41:33 +0900

    refactor: use yml settings

--- tasks/script.bash
+++ tasks/script.bash
@@ -3,36 +3,13 @@ tasks_script_install() {
   local secured_scripts="$DOTFILES_SECURED_CONFIG/script/files.conf"
   local corporate_config_path="$DF_CORPORATE_CONFIG_DIR/script/files.yml"
 
-  _script_run_scripts_yml "$DOTFILES_RESOURCES" "$config_path" 'main'
+  _script_run_scripts "$DOTFILES_RESOURCES" "$config_path" 'main'
   _script_run_scripts "$DOTFILES_SECURED_RESOURCES" "$secured_scripts" 'secure script conf'
-  _script_run_scripts_yml "$DF_CORPORATE_RESOURCES_DIR" "$corporate_config_path" 'corporate'
+  _script_run_scripts "$DF_CORPORATE_RESOURCES_DIR" "$corporate_config_path" 'corporate'
 }
+
 
 _script_run_scripts() {
-  local resources_dir=$1
-  local conf_path=$2
-  local conf_description=$3
-
-  echo -n "Checking if $conf_description exists ... "
-  if [ -f "$conf_path" ]; then
-    echo 'yes'
-
-    while read -u 9 script; do
-      if [ -z "$script" ]; then
-        continue
-      fi
-      if [ "${script:0:1}" = "#" ]; then
-        continue
-      fi
-
-      _script_run_script "$resources_dir" "$script"
-    done 9< "$conf_path"
-  else
-    echo 'no'
-  fi
-}
-
-_script_run_scripts_yml() {
   local resources_dir=$1
   local config_path=$2
   local repository=$3
@@ -50,6 +27,7 @@ _script_run_scripts_yml() {
     echo 'no'
   fi
 }
+
 
 _script_run_script() {
   local resources_dir="$1"
